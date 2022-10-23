918d4d3aa3c22a690f75bcfd50d04743726bbafc
RaymartBartilet <114345498+Raymartbartilet@users.noreply.github.com>
2022-10-23 12:48:54 +0800

    Add files via upload

---
+++ Hypotenuse.py
@@ -0,0 +1,10 @@
+from math import sqrt
+
+print("\n Please enter the length sides:" )
+
+Side1= float(input(" Side  is : "))
+Side2= float(input(" Side  is : "))
+
+Side3= sqrt(Side1*Side1 + Side2*Side2)
+
+print(" The length of the hypotenuse is : " , Side3 )
